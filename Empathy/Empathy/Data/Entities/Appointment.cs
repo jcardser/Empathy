1a0bb22d911940b81e66e3e1d5d38eb83a3ed504
JUAN SEBASTIAN CARDONA SERNA <jcardser@gmail.com>
2023-12-21 20:30:09 -0500

    Update Appointment Entity

--- Empathy/Empathy/Data/Entities/Appointment.cs
+++ Empathy/Empathy/Data/Entities/Appointment.cs
@@ -7,23 +7,77 @@ namespace Empathy.Data.Entities
     {
         public int Id { get; set; }
 
-        [Display(Name = "Fecha cita medica")]
+        [Display(Name = "Seleccione una fecha disponible")]
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime Date { get; set; }
 
-        [Display(Name = "Estado de la cita")]
-        public Boolean Status { get; set; }
+        //--ToDo Organizar...
 
-        [Display(Name = "Motivo de la cita médica")]
-        [MaxLength(200, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [Display(Name = "Cuéntanos el motivo de tu consulta.")]
+        [MaxLength(300, ErrorMessage = "El campo {0} debe tener máximo {1} carácteres")]
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Reason { get; set; }
 
-        //Conexión con Sede//
-        public ICollection<Appointment> Appointments { get; set; }
+        /* ::ESPACIO INFORMATIVO::
+         * En este espacio, se solicita el historial medico previo, tales como:
+         * 1. Alergias.
+         * 2. Medicamentos.
+         * 3. Cirugias.
+         * 4. Conmobilidades.
+         */
 
-        public ICollection<HealthCondition> HealthConditions { get; set; }  
+        [Display(Name ="Indicanos tus Alergias, Medicamentos que consumes, cirugias y/o conmorbilidades")]
+        [MaxLength(500, ErrorMessage ="El campo {0} debe tener máximo {1} carácteres")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string ConditionHistory { get; set; }
+
+        [Display(Name ="Antecedentes cardiacos familiares")]
+        [MaxLength(300, ErrorMessage ="El campo {1} debe tener máximo {1} carácteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool CardiacHistory {  get; set; }
+
+        [Display(Name = "Antecedentes presión arterial familiares")]
+        [MaxLength(300, ErrorMessage = "El campo {1} debe tener máximo {1} carácteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool PressureHistory { get; set; }
+
+        [Display(Name = "Antecedentes azúcar familiares")]
+        [MaxLength(300, ErrorMessage = "El campo {1} debe tener máximo {1} carácteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool SugarHistory { get; set; }
+
+        [Display(Name = "Indica tu peso (en kilogramos)")]
+        [MaxLength(10, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Weight { get; set; }
+
+        [Display(Name = "Indica tu estatura (en centímetros)")]
+        [MaxLength(10, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Height { get; set; }
+
+        [Display(Name = "¿Fumas?")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool Smoke { get; set; }
+
+        [Display(Name = "¿Consumes bebidas alcoholicas?")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool Beer { get; set; }
+
+        [Display(Name = "¿Te has fracturado alguna vez?")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public bool Fracture { get; set; }
+
+
+        //Estado de la cita lo actualiza el usuario. Funciona Como CRUD
+
+        [Display(Name = "Estado de la cita")]
+        public Boolean Status { get; set; }
+
+        //Conexión con Sede//
+        public ICollection<Sede> Sedes { get; set; }
+
 
     }
 }
--- Empathy/Empathy/Data/Entities/HealthCondition.cs
+++ Empathy/Empathy/Data/Entities/HealthCondition.cs
@@ -26,27 +26,7 @@ namespace Empathy.Data.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string EmergencyContact { get; set; }
 
-        [Display(Name = "Indica tu peso (en kilogramos)")]
-        [MaxLength(30, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public string Weight { get; set; }
-
-        [Display(Name = "Indica tu estatura (en centímetros)")]
-        [MaxLength(30, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public string Height { get; set; }
-
-        [Display(Name = "¿Fumas?")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public bool Smoke { get; set; }
-
-        [Display(Name = "¿Consumes bebidas alcoholicas?")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public bool Beer { get; set; }
-
-        [Display(Name = "¿Te has fracturado alguna vez?")]
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public bool Fracture { get; set; }
+       
 
         //Conexión a Cita Médica
         public Appointment Appointment { get; set; }
